9a30294781a8e323b69835e30a0c42b147861da7
Tim Satke <tim.satke@gmail.com>
2019-07-03 00:39:17 +0200

    Add godoc

--- internal/runtime/binding/environment_function.go
+++ internal/runtime/binding/environment_function.go
@@ -5,42 +5,64 @@ import (
 	"github.com/gojisvm/gojis/internal/runtime/lang"
 )
 
-type BindingStatus string
+// Status represents the status of a binding.
+// Can either be 'lexical', 'initialized', 'uninitialized'.
+type Status string
 
+// Available binding statuses.
 const (
-	BindingStatusLexical       BindingStatus = "lexical"
-	BindingStatusInitialized   BindingStatus = "initialized"
-	BindingStatusUninitialized BindingStatus = "uninitialized"
+	StatusLexical       Status = "lexical"
+	StatusInitialized   Status = "initialized"
+	StatusUninitialized Status = "uninitialized"
 )
 
 var _ Environment = (*FunctionEnvironment)(nil)
 
+// FunctionEnvironment is a declarative environment that is
+// ised to represent the top-level scope of a function and,
+// if the function is not an arrow function, provides a this
+// binding. If a function is not an arrow function and references
+// super, its function environment also contains the state that is
+// ised to perform super method invocations from within the function.
+// FunctionEnvironment is specified in 8.1.1.3.
 type FunctionEnvironment struct {
 	*DeclarativeEnvironment
 
-	ThisValue         lang.Value
-	ThisBindingStatus BindingStatus
-	FunctionObject    *lang.Object
-	HomeObject        lang.Value // Object or Undefined
-	NewTarget         lang.Value // Object or Undefined
+	// Used as 'this' for invocation of the function.
+	ThisValue lang.Value
+	// If the value is StatusLexical, this function is an
+	// arrow function and does not have a local this value.
+	ThisBindingStatus Status
+	// The function object whose invocation caused this
+	// environment to be created.
+	FunctionObject *lang.Object
+	// If the associated function has 'super' property
+	// accesses and is not an arrow function, HomeObject
+	// is the object that the function is bound to as a
+	// method. The default value for HomeObject is Undefined.
+	HomeObject lang.Value // Object or Undefined
+	// If this environment was created by the Construct-internal
+	// method, NewTarget is the value of the Construct-function's
+	// newTarget parameter. Otherwise, this value is Undefined.
+	NewTarget lang.Value // Object or Undefined
 }
 
 func (e *FunctionEnvironment) BindThisValue(val lang.Value) (lang.Value, errors.Error) {
-	if e.ThisBindingStatus == BindingStatusInitialized {
+	if e.ThisBindingStatus == StatusInitialized {
 		return nil, errors.NewReferenceError("'This' has already been initialized")
 	}
 
 	e.ThisValue = val
-	e.ThisBindingStatus = BindingStatusInitialized
+	e.ThisBindingStatus = StatusInitialized
 	return val, nil
 }
 
 func (e *FunctionEnvironment) HasThisBinding() bool {
-	return e.ThisBindingStatus != BindingStatusLexical
+	return e.ThisBindingStatus != StatusLexical
 }
 
 func (e *FunctionEnvironment) HasSuperBinding() bool {
-	if e.ThisBindingStatus == BindingStatusLexical {
+	if e.ThisBindingStatus == StatusLexical {
 		return false
 	}
 
@@ -48,7 +70,7 @@ func (e *FunctionEnvironment) HasSuperBinding() bool {
 }
 
 func (e *FunctionEnvironment) GetThisBinding() (lang.Value, errors.Error) {
-	if e.ThisBindingStatus == BindingStatusUninitialized {
+	if e.ThisBindingStatus == StatusUninitialized {
 		return nil, errors.NewReferenceError("'This' has not been initialized yet")
 	}
 
