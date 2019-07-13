422e90648e1b24a2f2d1533efd120282d0c7d349
Tim Satke <tim.satke@gmail.com>
2019-07-14 00:12:35 +0200

    Added test for inline object creation

--- vm_test.go
+++ vm_test.go
@@ -22,3 +22,28 @@ func TestHelloWorld(t *testing.T) {
 
 	golden.Equal(t, "TestHelloWorld", buf.Bytes())
 }
+
+func TestInlineObject(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	vm := gojis.NewVM()
+
+	var buf bytes.Buffer
+	vm.SetConsole(&buf)
+
+	vm.Eval(`
+function foo(arg) {
+	console.log(arg.func());
+}
+
+function gen(arg) {
+	return () => arg;
+}
+
+foo(gen({func: () => "Output"})());
+	`)
+
+	golden.Equal(t, "TestInlineObject", buf.Bytes())
+}
