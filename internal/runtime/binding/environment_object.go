60f922bbc40da144cc0457731c403aaf6c1d016f
Tim Satke <tim.satke@gmail.com>
2019-07-14 12:23:35 +0200

    Add godoc

--- internal/runtime/binding/environment_object.go
+++ internal/runtime/binding/environment_object.go
@@ -7,6 +7,25 @@ import (
 
 var _ Environment = (*ObjectEnvironment)(nil)
 
+// ObjectEnvironment is associated with an object called its binding object. An
+// object Environment Record binds the set of string identifier names that
+// directly correspond to the property names of its binding object. Property
+// keys that are not strings in the form of an IdentifierName are not included
+// in the set of bound identifiers. Both own and inherited properties are
+// included in the set regardless of the setting of their [[Enumerable]]
+// attribute. Because properties can be dynamically added and deleted from
+// objects, the set of identifiers bound by an object Environment Record may
+// potentially change as a side-effect of any operation that adds or deletes
+// properties. Any bindings that are created as a result of such a side-effect
+// are considered to be a mutable binding even if the Writable attribute of the
+// corresponding property has the value false. Immutable bindings do not exist
+// for object Environment Records. Object Environment Records created for
+// withwith statements (13.11) can provide their binding object as an implicit
+// this value for use in function calls. The capability is controlled by a
+// withEnvironment Boolean value that is associated with each object Environment
+// Record. By default, the value of withEnvironment is false for any object
+// Environment Record.
+// ObjectEnvironment is specified in 8.1.1.2.
 type ObjectEnvironment struct {
 	outer Environment
 
